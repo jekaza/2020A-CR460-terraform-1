921b2365869ec16067f5956d29efd90ff511f5f5
jekaza <70776240+jekaza@users.noreply.github.com>
2020-09-30 13:33:17 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,41 +1,41 @@
-resource "google_compute_network" "cr460demo" {
-  name                    = "cr460demo"
+resource "google_compute_network" "cr460devoir1" {
+  name                    = "cr460devoir1"
   auto_create_subnetworks = "false"
 }
 
 
-resource "google_compute_subnetwork" "mtl-dmz" {
-  name          = "mtl-dmz"
-  ip_cidr_range = "172.16.1.0/24"
+resource "google_compute_subnetwork" "prod-dmz" {
+  name          = "prod-dmz"
+  ip_cidr_range = "172.16.3.0/24"
   region        = "us-east1"
-  network       = google_compute_network.cr460demo.self_link
+  network       = google_compute_network.cr460devoir1.self_link
 }
 
-resource "google_compute_subnetwork" "mtl-workload" {
-  name          = "mtl-workload"
+resource "google_compute_subnetwork" "prod-workload" {
+  name          = "prod-workload"
   ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.cr460demo.self_link
+  network       = google_compute_network.cr460devoir1.self_link
   region        = "us-east1"
 }
 
-resource "google_compute_subnetwork" "mtl-internal" {
-  name          = "mtl-internal"
+resource "google_compute_subnetwork" "prod-internal" {
+  name          = "prod-internal"
   ip_cidr_range = "10.0.2.0/24"
   region        = "us-east1"
-  network       = google_compute_network.cr460demo.self_link
+  network       = google_compute_network.cr460devoir1.self_link
 }
 
-resource "google_compute_subnetwork" "mtl-backend" {
-  name          = "mtl-backend"
+resource "google_compute_subnetwork" "prod-backend" {
+  name          = "prod-backend"
   ip_cidr_range = "10.0.3.0/24"
-  network       = google_compute_network.cr460demo.self_link
+  network       = google_compute_network.cr460devoir1.self_link
   region        = "us-east1"
 }
 
 
 resource "google_compute_firewall" "ssh-public" {
   name    = "ssh-public"
-  network = google_compute_network.cr460demo.name
+  network = google_compute_network.cr460devoir1.name
   allow {
     protocol = "tcp"
     ports    = ["22"]
@@ -45,7 +45,7 @@ resource "google_compute_firewall" "ssh-public" {
 
 resource "google_compute_firewall" "web-public" {
   name    = "web-public"
-  network = google_compute_network.cr460demo.name
+  network = google_compute_network.cr460devoir1.name
   allow {
     protocol = "tcp"
     ports    = ["80", "443"]
@@ -55,7 +55,7 @@ resource "google_compute_firewall" "web-public" {
 
 resource "google_compute_firewall" "ssh-workload" {
   name    = "ssh-workload"
-  network = google_compute_network.cr460demo.name
+  network = google_compute_network.cr460devoir1.name
   allow {
     protocol = "tcp"
     ports    = ["22"]
@@ -65,18 +65,18 @@ resource "google_compute_firewall" "ssh-workload" {
 
 resource "google_compute_firewall" "internal-control" {
   name    = "internal-control"
-  network = google_compute_network.cr460demo.name
+  network = google_compute_network.cr460devoir1.name
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "2379", "2380"]
+    ports    = ["22", "2846", "5462"]
   }
 
   source_ranges = ["172.16.1.0/24", "10.0.1.0/24" ]
   target_tags = ["backend"]
 }
 
-
+/*
 resource "google_dns_record_set" "jump" {
   name = "jump.cloudlab.matbilodeau.dev."
   type = "A"
@@ -94,6 +94,6 @@ resource "google_dns_record_set" "vault" {
   ttl  = 300
 
   managed_zone = "cloudlab"
-
+*/  
   rrdatas = [google_compute_instance.vault.network_interface.0.access_config.0.nat_ip]
 }
